c724e57a40094a5aba68780c43d039b21cc03a0c
LeninSG21 <lenin.sigu14@gmail.com>
2020-04-23 14:57:32 -0500

    Can read hidraw from the controller

--- Joystick/ps4.py
+++ Joystick/ps4.py
@@ -1,32 +1,45 @@
 import serial
 import os
-import time
-
 import struct 
 
-# struct js_event {
-# 		__u32 time;     /* event timestamp in milliseconds */
-# 		__s16 value;    /* value */
-# 		__u8 type;      /* event type */
-# 		__u8 number;    /* axis/button number */
-# 	};
+"""
+byte index
 
-f = open( "/dev/input/event22", "rb" ); 
+0 --> report index
+1 --> L X axis
+2 --> L Y axis
+3 --> R X axis
+4 --> R Y axis
+5 --> triangle << 7 | circle << 6 | cross << 5 | square << 4 || D-PAD: hat format
+                                                                0x08 --> released
+                                                                0x07 --> NW
+                                                                0x06 --> W
+                                                                0x05 --> SW
+                                                                0x04 --> S
+                                                                0x03 --> SE
+                                                                0x02 --> E
+                                                                0x01 --> NE
+                                                                0x00 --> N
+6 --> R3 << 7 | L3 << 6 | Options << 5 | Share << 4 | R2 << 3 | L2 << 2 | R1 << 1 | L1
+7 --> Counter << 2 | T-PAD click << 1 | PS-Button
+8 --> L2 Trigger
+9 --> R2 Trigger
+12 --> Battery Level
+
+Check full HID Data Format for DS4 @ https://www.psdevwiki.com/ps4/DS4-USB
+"""
+
+f = open("/dev/hidraw2", "rb")
 # Open the file in the read-binary mode
 while 1:
 
-    data = f.read(8)
-    # print len(data)
-    timestamp = data[:4]
-    # print len(timestamp)
-    other = data[4:]
-    print struct.unpack('I', timestamp)
-    print struct.unpack('hBB', other)
-    # timestamp =  struct.unpack('I',f.read(4))
-    # value = struct.unpack('h', f.read(2))
-    # type_ = struct.unpack('B', f.read(1))
-    # number = struct.unpack('B',f.read(1))
-    # print("Timestamp: " + str(timestamp))
-    # print("Value: " + str(value))
-    # print("Type: " + str(type_))
-    # print("Number: " + str(number))
+    data = f.read(64)
+
+    leftStickX = struct.unpack('B', data[1])
+    leftStickY = struct.unpack('B', data[2])
+    rightStickX = struct.unpack('B', data[3])
+    rightStickY = struct.unpack('B', data[4])
+    triangle = data[5] & 7
+
+
+    print 
