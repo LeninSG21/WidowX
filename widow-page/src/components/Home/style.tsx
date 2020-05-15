956207456d34fbef54ef6b7e1458330bf0c2faaa
Lenin Silva <lenin.silva@softtek.com>
2020-05-14 22:10:41 -0500

    Added home. Need to work on drawer and then fill with info

---
+++ widow-page/src/components/Home/style.tsx
@@ -0,0 +1,37 @@
+import { makeStyles } from "@material-ui/core";
+
+const drawerWidth = 240;
+
+export const useStyles = makeStyles((theme) => ({
+	root: {
+		display: "flex",
+	},
+	drawer: {
+		[theme.breakpoints.up("sm")]: {
+			width: drawerWidth,
+			flexShrink: 0,
+		},
+	},
+	drawerPaper: {
+		width: drawerWidth,
+	},
+	appBar: {
+		[theme.breakpoints.up("sm")]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: drawerWidth,
+		},
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up("sm")]: {
+			display: "none",
+		},
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	select: {
+		color: theme.palette.common.white,
+		marginRight: theme.spacing(1),
+	},
+}));
