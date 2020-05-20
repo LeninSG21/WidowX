54429be54642c3a33048d1a98e2245a1bae72aee
Lenin Silva <lenin.silva@softtek.com>
2020-05-20 12:47:38 -0500

    deleted widow-page

--- widow-page/src/components/DK/markdown.ts
+++
@@ -1,4 +0,0 @@
-export const spanishFK = `hola`;
-export const englishFK = `# Forward Kinematics
-    AS you imagine, I hope this works
-`;
--- widow-page/src/components/Home/view.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const HomeView: React.FC = () => {
-	return <h1>This is my swamp</h1>;
-};
